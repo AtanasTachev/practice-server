6f843a6e7cb1a7ac8da063ad0a6e049d9cf8d3a1
Atanas Tachev <tachev_a@yahoo.com>
2022-10-06 16:41:36 +0300

    add controller

--- src/controllers/breweryController.js
+++ src/controllers/breweryController.js
@@ -3,5 +3,12 @@ const breweryService = require('../services/breweryService')
 
 router.post('/create', async(req, res) => {
     let {brewery, user, address, product, quantity} = req.body
-    
+    try {
+        let order = await breweryService.createOrder(brewery, user, address, product, quantity)
+        res.status(200).json(order)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
+
+module.exports = router
--- src/controllers/practiceController.js
+++ src/controllers/practiceController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const practiceService = require('../services/practiceService')
 
 router.post('/create', async(req, res) => {
-    let {practiceTitle, mentor, startDate, duration, dateOfExam, dueDateOfProject,creator} = req.body;
+    let {practiceTitle, mentor, startDate, duration, dateOfExam, dueDateOfProject, creator} = req.body;
     try{
         let practice = await practiceService.createPractice(practiceTitle, mentor, startDate, duration, dateOfExam, dueDateOfProject, creator)
         res.status(200).json(practice);
